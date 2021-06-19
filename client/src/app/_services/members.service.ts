afcb5141e43a26ffcb8d5701c83c415c8d3825a8
JavierBuffa <javibuffa92@gmail.com>
2021-06-19 16:59:25 -0400

    Section 13

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -16,12 +16,7 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    if (this.members.length > 0) return of(this.members);
-    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members => {
-        this.members = members;
-        return members;
-    }))
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe()
   }
 
   getMember(username: string) {
